7a94839db7751adface9fe2d531acf661dfee2f2
Жунусов Эльнур Ильдарович <elnur.zhunussov@ffin.life>
2023-09-29 16:46:06 +0600

    Add: Added form (email, password, role, t&c)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  
+    <signup-form-vue/>
 </template>
 
 <script>
-export default {
+import SignupFormVue from './components/SignupForm.vue'
 
+export default {
+    components: {SignupFormVue},
 }
 </script>
 
 <style>
+#app {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
 
+body {
+    margin: 0;
+    background: #eee;
+}
 </style>
